435115033e0b74b4060d300f64a6a10419f2bd3b
Gustavo Spiess <gustavospiess@gmail.com>
2017-05-06 23:04:53 -0300

    save for test

--- src/geneticPie.py
+++ src/geneticPie.py
@@ -1,46 +1,68 @@
 import utils
-from utils import Configurable, Printer
+from utils import *
 
-class GeneticTypeGroup(Configurable):
+class Gen(Configurable):
 
-	#inherited
-	def get_configs(self):
-		return {'name' : {'default' : ''},
-				'desc' : {'default' : ''},
-				'list' : {'default' : []}}
+    #inherited
+    def get_req_configs(self):
+        return {
+        'req_gen' : {
+            'default' : {},
+            'desc' : 'A dict of names and required genetic types'
+            }
+        }
 
-class GeneticType(Configurable):
+    #public
+    def is_mutable(*param):
+        raise Exception('not implemented')
 
-	#inherited
-	def get_configs(self):
-		return {'name' : {'default' : ''},
-				'desc' : {'default' : ''},
-				'gens_down' : {'default' : []},
-				'gens_up' : {'default' : []}}
+    #public
+    def mutate(*param):
+        raise Exception('not implemented')
 
-class GeneticType(Configurable):
+    #public
+    def is_crossoverble(*param):
+        raise Exception('not implemented')        
 
-	#inherited
-	def get_configs(self):
-		return {'name' : {'default' : ''},
-				'desc' : {'default' : ''},
-				'gens_down' : {'default' : []},
-				'gens_up' : {'default' : []}}
+    #public
+    def crossover(*param):
+        raise Exception('not implemented')
 
 class Individual(Configurable):
 
-	#inherited
-	def get_configs(self):
-		return {'Gens' : {}}
+    def __init__(self, config = {}):
+        super(Individual, self).__init__(config)
+        map (lambda c : config.pop(c), self.keys())
+        self.update(config)
+        for g in self['gens']:
+            if g in self.keys():
+                for r in self[g]['req_gen'].keys():
+                    self[r] = self[g]['req_gen'][r]
 
 
-class Simulation(Configurable):
+    #inherited
+    def get_req_configs(self):
+        return {
+            'gens' : {
+                'default' : [], 
+                'desc' : 'A list of what other configs are gens'
+            }
+        }
 
-	#inherited
-	def get_configs(self):
-		return {'name' : {'default' : ''},
-				'desc' : {'default' : ''},
-				'pool' : {'default' : []},
-				'popu' : {'default' : []}}
 
-a = 
+class Simulation(Descriptble):
+
+    #inherited
+    def get_req_configs(self):
+        req = super(Simulation, self).get_req_configs()
+        req.update({
+            'pool' : {
+                'default' : [],
+                'desc' : ''
+            },
+            'popu' : {
+                'default' : [],
+                'desc' : 'list of'
+            }
+        })
+        return req
