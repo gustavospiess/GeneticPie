da4cdcace3feda401444624963256f8d9eeebd6e
Gustavo Spiess <gustavospiess@gmail.com>
2017-07-25 22:05:59 -0300

    clean up and adds

--- src/geneticPie.py
+++ src/geneticPie.py
@@ -1,5 +1,4 @@
-import utils
-from utils import *
+import random
 
 '''
 Representation of an changeble information for an possible responce.
@@ -21,24 +20,18 @@ class Gen():
             raise Exception('req_gens must be a dict')
 
     #public
-    def get_req_gens(self):
-        return self.req_gens
-
-    #public
-    def is_mutable(self):   
-        return len(self.mutation_list) > 0
-
-    #public
     def mutate(self):
-        raise Exception('not implemented')
-
+        if (len(mutation_list) == 1):
+            mutation_list[0].mutate(self)
+        elif (len(mutation_list) > 1):
+            random.sample(mutation_list, 1)[0].mutate(gen)
     #public
     def new_instace(self):
         return self.__class__(self.get_new_instace_param())
 
     #protected
     def get_new_instace_param(self):
-        return {'mutation_list':mutation_list, 'req_gens':req_gens}
+        return {'mutation_list':self.mutation_list, 'req_gens':self.req_gens}
 
 '''
 Gen that has a value
@@ -52,7 +45,7 @@ class ValuableGen(Gen):
 
     #public
     def new_instace(self):
-        return self.__class__(self.get_new_instace_param(), self.value)
+        return self.__class__(self.get_new_instace_param(), value = self.value)
 
 '''
 Gen that implements an run method
@@ -60,14 +53,13 @@ Gen that implements an run method
 class RunnableGen(Gen):
 
     #public
-    def __init__(self, param, individual):
-        self.individual = None
-        if param and param[0]:
-            self.individual = param[0]
+    def __init__(self, param, individual = None):
+        self.individual = individual
+        Gen.__init__(self, param)
 
     #public
     def new_instace(self):
-        return self.__class__(self.get_new_instace_param(), self.individual)
+        return self.__class__(self.get_new_instace_param(), individual =  self.individual)
 
     #public
     def run(self, param):
@@ -80,30 +72,36 @@ class Individual():
 
     #public 
     def __init__(self, gens):
-        if len((x for x in gens.values() if not issubclass(x.__class__, Gen))) > 0:
+        if len([x for x in gens.values() if not issubclass(x.__class__, Gen)]) > 0:
             raise Exception('gens must extend Gen') 
 
         self.gens = gens
         for g in self.gens.values():
-            for gen_name, new_instace in g.get_req_gens().items():
+            for gen_name, new_instace in g.req_gens.items():                
                 instance = new_instace()
-                if not gen_name in self.gens.keys() or issubclass(self.gens[gen_name].__class__, instance.__class__):
-                    self.gens[gen_name] = instance
+                instance_name = gen_name
+                i = 0
+                while instance not in self.gens.values():
+                    instance_name = gen_name + ("_"+str(i) if i else "")
+                    if not instance_name in self.gens.keys() or issubclass(self.gens[instance_name].__class__, instance.__class__):
+                        self.gens[instance_name] = instance
+                    else:
+                        i = i + 1
+                if instance_name != gen_name:
+                    g.req_gens[instance_name] = g.req_gens.pop(gen_name)
+
         for x in(x for x in self.gens.values() if issubclass(g.__class__, RunnableGen)):
             g.individual = self
-
 
     #public
     def calculate_fitness(self, param):
         raise Exception('not implemented')
 
     #public
-    def is_crossoverble(self, partner):
-        raise Exception('not implemented')        
-
-    #public
     def crossover(self, partner):
-        raise Exception('not implemented')
+        if not issubclass(partner.__class__, Individual):
+            raise Exception("partner must be an Individual")
+        return self.__class__({**self.gens, **partner.gens})
 
     #public
     def new_instace(self):
@@ -131,6 +129,4 @@ class Simulation():
         self.population.sort(cmp = sub_sorter)
         return sel
 
-class testGen(ValuableGen):
-    def __init__(self, param):
-        ValuableGen.__init__(self, param)
+help(Gen)
