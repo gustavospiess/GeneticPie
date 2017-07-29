19cf36198d0ffa64e6ef4212f633b18e78d2cd66
Gustavo Spiess <gustavospiess@gmail.com>
2017-07-28 23:23:25 -0300

    Made it work again
    at this very moment, we'r able to find functions for lines from the points in the graphic... =D
    
    By the way, still in development.

--- src/FirstGradeFunctionsearcher.py
+++ src/FirstGradeFunctionsearcher.py
@@ -1,75 +1,29 @@
 import geneticPie
 from geneticPie import *
-import random
-from random import *
-
-class FloatGen(ValuableGen):
-
-    #public
-    def __init__(self, *param):
-        ValuableGen.__init__(self, param[0])
-        self.default = self.value
-
-    #public
-    def get_req_gens(self):
-        return {}
-
-    #public
-    def is_mutable(self, *param):
-        return True
-
-    #public
-    def mutate(self, *param):
-        percent = int(random() * 100)
-        mutatio = random() + 0.5
-        if percent <= 9:
-            self.value = {
-                0 : self.default,
-                1 : self.value - mutatio,
-                2 : self.value + mutatio,
-                3 : self.value * mutatio,
-                4 : self.value / mutatio,
-                5 : int(self.value),
-                6 : int(self.value) + 0.5,
-                7 : int(self.value) - 0.5,
-                8 : int(self.value) + 0.1,
-                9 : int(self.value) - 0.1
-            }[percent]
-
-    def new_instace(self):
-        return self.__class__(self.value)
 
 class FuncFGen(RunnableGen):
 
-    #public
-    def run(self, *param):
-        return self.individual.gens['a'].value * param[0] + self.individual.gens['b'].value
+    def __init__(self, param):
+        if 'req_gens' not in param.keys():
+            param['req_gens'] = {'a' : self.get_float_gen, 'b' : self.get_float_gen}
+        RunnableGen.__init__(self, param)
 
     #public
-    def get_req_gens(self):
-        return {'a' : self.get_float_gen, 'b' : self.get_float_gen}
+    def run(self, param):
+        return self.individual.gens['a'].value * param + self.individual.gens['b'].value
 
     #private
     def get_float_gen(self):
-        return FloatGen(0)
-
-    #public
-    def is_mutable(self, *param):
-        return False
+        return geneticPie.Default.IntGen({})
 
 class FuncFInd(Individual):
 
     #public
     def __init__(self, gens):
-        if gens:
-            g = gens
-        else:
-            g = {}
-        g['main'] = FuncFGen(self)
-        Individual.__init__(self, g)
+        Individual.__init__(self, {'main': FuncFGen({}), **gens})
 
     #public
-    def calculate_fitness(self, *param):
+    def calculate_fitness(self, param):
         total = 0
         for sub_param in param:
             parcial = self.gens['main'].run(sub_param[0])
@@ -81,9 +35,9 @@ class FuncFInd(Individual):
 
         return total
 
-pars = [(0, 0), (20,20)]
+pars = [(1, 12), (2,15)]
 sim = Simulation()
-sim.population.append(FuncFInd(None))
+sim.population.append(FuncFInd({}))
 
 for i in range(9):
     sim.population.append(sim.population[0].new_instace())
@@ -91,14 +45,15 @@ for i in range(9):
 for k in range(5000):
     for individual in sim.population:
         for g in individual.gens.values():
-            if g.is_mutable():
-                g.mutate()
-    sim.sort_by_fitness(*pars)
+            g.mutate()
+    sim.sort_by_fitness(pars)
     for k in range(5):
         sim.population[k+5] = sim.population[k].new_instace()
 
-        
-
-#print map((lambda x : (x.calculate_fitness((4, 4)))),sim.population)
+sim.sort_by_fitness(pars)
 for ind in sim.population:
-    print ind.calculate_fitness(*pars), 'a = ', ind.gens['a'].value, 'b = ', ind.gens['b'].value
+    print((((str(ind.gens['a'].value) if ind.gens['a'].value != 1 else "") + 
+        'x') if ind.gens['a'].value else "" ) + 
+        ('+' if ind.gens['b'].value > 0 else "") + 
+        (str(ind.gens['b'].value) if ind.gens['b'].value else ""))
+    print(' ( erro aproximado de', ind.calculate_fitness(pars), ')')
--- src/geneticPie.py
+++ src/geneticPie.py
@@ -6,13 +6,16 @@ Representation of an changeble information for an possible responce.
 class Gen():
 
     #public
-    class mutation():
+    class Mutation():
+        def __init__(self, param):
+            self.mutate = param['mutate']
+
         def mutate(self, gen):
             raise Exception('not implemented')
 
     #public
     def __init__(self, param):
-        self.mutation_list = param['mutation_list'] if 'mutation_list' in param else []
+        self.mutation_list = param['mutation_list'] if 'mutation_list' in param.keys(   ) else []
         self.req_gens = param['req_gens'] if 'req_gens' in param else {}
         if self.mutation_list.__class__ != list:
             raise Exception('mutation_list must be a list')
@@ -21,17 +24,14 @@ class Gen():
 
     #public
     def mutate(self):
-        if (len(mutation_list) == 1):
-            mutation_list[0].mutate(self)
-        elif (len(mutation_list) > 1):
-            random.sample(mutation_list, 1)[0].mutate(gen)
+        if random.randint(0,9) == 0:
+            if (len(self.mutation_list) == 1):
+                self.mutation_list[0].mutate(self)
+            elif (len(self.mutation_list) > 1):
+                random.choice(self.mutation_list).mutate(self)
     #public
     def new_instace(self):
-        return self.__class__(self.get_new_instace_param())
-
-    #protected
-    def get_new_instace_param(self):
-        return {'mutation_list':self.mutation_list, 'req_gens':self.req_gens}
+        return self.__class__(self.__dict__)
 
 '''
 Gen that has a value
@@ -39,13 +39,9 @@ Gen that has a value
 class ValuableGen(Gen):
 
     #public
-    def __init__(self, param, value = None):
+    def __init__(self, param):
         Gen.__init__(self, param)
-        self.value = value if value else param['value'] if 'value' in param else value
-
-    #public
-    def new_instace(self):
-        return self.__class__(self.get_new_instace_param(), value = self.value)
+        self.value = param['value'] if 'value' in param.keys() else None
 
 '''
 Gen that implements an run method
@@ -53,13 +49,13 @@ Gen that implements an run method
 class RunnableGen(Gen):
 
     #public
-    def __init__(self, param, individual = None):
-        self.individual = individual
+    def __init__(self, param):
+        self.individual = param['individual'] if 'individual' in param.keys() else None
         Gen.__init__(self, param)
 
     #public
     def new_instace(self):
-        return self.__class__(self.get_new_instace_param(), individual =  self.individual)
+        return self.__class__(self.__dict__)
 
     #public
     def run(self, param):
@@ -70,28 +66,47 @@ Representation of an possible response.
 '''
 class Individual():
 
-    #public 
+    #public
     def __init__(self, gens):
+        #gens = {k : v() if issubclass(self.new_instace.__class__, v.__class__) else v for k, v in gens.items()}
         if len([x for x in gens.values() if not issubclass(x.__class__, Gen)]) > 0:
-            raise Exception('gens must extend Gen') 
+            raise Exception('gens must extend Gen')
+
+        self.gens = {}
+
+        def add_gen(self, gens):
+            adds = {}
+            for g in gens.values():
+                for gen_name, new_instace in g.req_gens.items():
+                    instance = new_instace()
+                    instance_name = gen_name
+                    if (instance_name in self.gens.keys() and
+                        issubclass(self.gens[instance_name].__class__, instance.__class__)):
+                        continue
+                    i = 0
+                    while instance not in self.gens.values() and instance not in adds.values():
+                        instance_name = gen_name + ("_"+str(i) if i else "")
+                        if (not instance_name in self.gens.keys() or
+                            issubclass(self.gens[instance_name].__class__, instance.__class__)):
+                            adds[instance_name] = instance
+                        else:
+                            if issubclass(instance.__class__, self.gens[instance_name].__class__):
+                                break
+                            i = i + 1
+                    if instance_name != gen_name:
+                        g.req_gens[instance_name] = g.req_gens.pop(gen_name)
+            return adds
 
         self.gens = gens
-        for g in self.gens.values():
-            for gen_name, new_instace in g.req_gens.items():                
-                instance = new_instace()
-                instance_name = gen_name
-                i = 0
-                while instance not in self.gens.values():
-                    instance_name = gen_name + ("_"+str(i) if i else "")
-                    if not instance_name in self.gens.keys() or issubclass(self.gens[instance_name].__class__, instance.__class__):
-                        self.gens[instance_name] = instance
-                    else:
-                        i = i + 1
-                if instance_name != gen_name:
-                    g.req_gens[instance_name] = g.req_gens.pop(gen_name)
+        adds = None
+        first = True
+        while first or adds:
+            first = False
+            adds = add_gen(self, gens)
+            self.gens = {**self.gens, **adds}
 
-        for x in(x for x in self.gens.values() if issubclass(g.__class__, RunnableGen)):
-            g.individual = self
+        for w in(x for x in self.gens.values() if issubclass(x.__class__, RunnableGen)):
+            w.individual = self
 
     #public
     def calculate_fitness(self, param):
@@ -105,7 +120,7 @@ class Individual():
 
     #public
     def new_instace(self):
-        return self.__class__({ k : v.new_instace for k, v in self.gens.items()})
+        return self.__class__({ k : v.new_instace() for k, v in self.gens.items()})
 
 class Simulation():
 
@@ -115,18 +130,42 @@ class Simulation():
 
     #public
     def sort_by_fitness(self, param):
-        def sorter(x, y):
-            if x > y:
-                return 1
-            else:
-                if x < y:
-                    return -1
-            return 0
+        def key(ind):
+            return ind.calculate_fitness(param)
 
-        def sub_sorter(ind_x, ind_y):
-            return sorter(ind_x.calculate_fitness(param), ind_y.calculate_fitness(param))
+        self.population.sort(key = key)
+        return self
 
-        self.population.sort(cmp = sub_sorter)
-        return sel
+class Default():
+    def add_1(gen):
+        gen.value += 1
+    def add_10(gen):
+        gen.value += 10
+    def add_100(gen):
+        gen.value += 100
+    def sub_1(gen):
+        gen.value -= 1
+    def sub_10(gen):
+        gen.value -= 10
+    def sub_100(gen):
+        gen.value -= 100
+    def mul_1(gen):
+        gen.value *= 1
+    def mul_10(gen):
+        gen.value *= 10
+    def mul_100(gen):
+        gen.value *= 100
 
-help(Gen)
+    DefaultIntMutation = [Gen.Mutation({'mutate' : add_1}),
+        Gen.Mutation({'mutate' : sub_1}),
+        Gen.Mutation({'mutate' : mul_1}),
+        Gen.Mutation({'mutate' : add_10}),
+        Gen.Mutation({'mutate' : sub_10}),
+        Gen.Mutation({'mutate' : mul_10}),
+        Gen.Mutation({'mutate' : add_100}),
+        Gen.Mutation({'mutate' : sub_100}),
+        Gen.Mutation({'mutate' : mul_100})]
+    #public
+    class IntGen(ValuableGen):
+        def __init__(self, param):
+            ValuableGen.__init__(self, {'mutation_list' : Default.DefaultIntMutation, 'value': 0, **param})
